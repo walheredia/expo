05e6abe6fa5c86b1136f074d60b2b3cd9e38bf96
Walter Heredia <wal_dh@hotmail.com>
2016-01-13 23:58:54 -0300

    it doesn't work yet

--- app/controllers/StockController.php
+++ app/controllers/StockController.php
@@ -1,13 +1,15 @@
 <?php
 	class StockController extends BaseController {
 		public function getEditStock($id_articulo){
-			 $articulo = DB::table('articulos')
-			 	->where('id_articulo', '=', $id_articulo)
-	            //->join('stock', 'articulos.id_articulo', '=', 'stock.id_articulo')
-	            //->select('articulos.id_articulo','articulos.nombre')
-	            ->get();
-			var_dump($articulo);
-
+			$articulo = DB::table('articulos')
+		        ->join('stock', function($join)
+		        {
+		        	$art = $id_articulo;
+		            $join->on('articulos.id_articulo', '=', 'stock.id_articulo')
+		                 ->where('articulos.id_articulo', '=', $art);
+		        })
+		        ->get();
+		        var_dump($articulo);
 			//$sucursales = Sucursal::all();
 			//return View::make('edit_stock')->with('sucursales',$sucursales);
 		}
